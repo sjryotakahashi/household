5b64c3e5f4a6c1d7188c80ee6d95432f68dfce27
ryotakahashi <sjrt33m@ezweb.ne.jp>
2016-10-08 09:05:59 +0000

    merge user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
   end
   
   def set_params
