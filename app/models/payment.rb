5b9ed1182fd895420f8a66030f61932205e0282a
ryotakahashi <sjrt33m@ezweb.ne.jp>
2016-10-13 11:57:13 +0000

    household-5

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,7 @@
 class Payment < ActiveRecord::Base
+  validates :settlement_date, presence: true
+  validates :balance, presence: true
+  validates :description, presence: true, length: { maximum: 30 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
   belongs_to :user
 end
