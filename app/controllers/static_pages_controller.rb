9f4ac0a991bc857d9739987db9f779b86d916891
ryotakahashi <sjrt33m@ezweb.ne.jp>
2016-10-14 07:35:59 +0000

    household-6

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    #@payments = Payment.all
+    @q        = Payment.search(params[:q])
+    @payments = @q.result(distinct: true)
   end
+  
+  
 end
